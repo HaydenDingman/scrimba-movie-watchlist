9ecb71fb7871deba118541e1c64079e7de706e85
Hayden Dingman <haydendingman@gmail.com>
2025-06-04 14:47:41 -0700

    Render API calls to page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const filmSearchInput = document.getElementById("movie-search");
+const filmSearchInput = document.getElementById("film-search");
+const filmContainer = document.getElementById("film-container");
 
 document.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -6,16 +7,53 @@ document.addEventListener("submit", (e) => {
 })
 
 async function fetchSearchResults() {
+    filmContainer.innerHTML = `<p class="placeholder-text">Fetching results...</p>`
+    
     const res = await fetch(`http://www.omdbapi.com/?apikey=32eb1eab&s=${filmSearchInput.value}&plot=full`);
     const films = await res.json();
-    console.log(films);
-    films.Search.map(film => renderSearchResults(film));
+    
+    const filmArray = films.Search.map(film => film.imdbID);
+    let filmHtml = "";
+    
+    for (let filmToSearch of filmArray) {
+        filmHtml += await renderSearchResults(filmToSearch);
+    }
+    
+
+    // const filmHtml = await Promise.all(films.Search.map(async (filmToSearch) => await renderSearchResults(filmToSearch)));
+    // filmHtml.join("");
+    console.log(`filmHtml: ${await filmHtml}`);
+    filmContainer.innerHTML = await filmHtml;
 }
 
-function renderSearchResults(film) {
-    fetch(`http://www.omdbapi.com/?apikey=32eb1eab&i=${film.imdbID}&plot=short`)
-        .then(res => res.json())
-        .then(filmDetails => {
-            console.log(filmDetails);
-        })
+async function renderSearchResults(filmToSearch) {
+    const res = await fetch(`http://www.omdbapi.com/?apikey=32eb1eab&i=${filmToSearch}&plot=short`);
+    const film = await res.json();
+        
+    const html = `<div class="film-card" id="${film.imdbID}">
+                <img src="${film.Poster}" alt="Poster for ${film.Title}">
+                <div class="film">
+                    <div>
+                        <h2 class="film-title">${film.Title}</h2>
+                        <div class="rating">
+                            <i class="fa-solid fa-star"></i>
+                            <p class="film-rating">${film.imdbRating}</p>
+                        </div>
+                    </div>
+                    <div class="film-details">
+                        <p class="film-runtime">${film.Runtime}</p>
+                        <p class="film-genres">${film.Genre}</p>
+                        <div class="add-to-watchlist" data-id="${film.imdbID}">
+                            <i class="fa-solid fa-square-plus"></i>
+                            <p>Add to watchlist</p>
+                        </div>
+                    </div>
+                    <p class="film-plot">${film.Plot}</p>
+                </div>
+            </div>
+            <hr />
+            `
+
+    console.log(html);
+    return html;
 }
